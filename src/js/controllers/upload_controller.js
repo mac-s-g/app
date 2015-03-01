ee3a4e05ef1d2d5e59b450319072aaf9986d982a
Taylor Birkeland <tabirkeland@gmail.com>
2015-03-01 21:25:46 +0000

    merging

--- src/js/controllers/upload_controller.js
+++ src/js/controllers/upload_controller.js
@@ -1,8 +1,16 @@
 (function () {
     var app = angular.module('App.controllers.Upload', []);
-    app.controller('UploadController', function() {
+    app.controller('UploadController', ['$scope', function($scope) {
+        this.messaging_campaigns = [
+            {id : 'Messaging 1'},
+            {id : 'Messaging 2'},
+            {id : 'Messaging 3'}
+         ];
+
+        this.lead = {};
+
         this.uploadLead = function () {
-            console.log('SUBMIT');
+            console.log(this.lead);
         }
-    });
+    }]);
 })();
