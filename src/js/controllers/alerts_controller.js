f9796093b820c4c3b35d0651c2aab9f36931b4dd
Taylor Birkeland <tabirkeland@gmail.com>
2015-03-02 04:21:00 +0000

    Merging

--- src/js/controllers/alerts_controller.js
+++ src/js/controllers/alerts_controller.js
@@ -1,6 +1,11 @@
 (function () {
     var app = angular.module('App.controllers.Alerts', []);
-    app.controller('AlertsController', function() {
-    	this.message = 'Alerts Page';
-    }); 
+    app.controller('AlertsController', ['$http', function($http) {
+    	var that = this;
+    	this.response = '';
+
+		$http.get("http://54.69.7.81/taylor/html/app_api/methods/Get/")
+    		.success(function(response) {that.response = response;});
+
+    }]); 
 })();
--- src/js/controllers/upload_controller.js
+++ src/js/controllers/upload_controller.js
@@ -1,22 +1,28 @@
 (function () {
     var app = angular.module('App.controllers.Upload', []);
     app.controller('UploadController', ['$scope', function($scope) {
+        $scope.submitted = false;
         $scope.lead = {};
 
-        $scope.messaging_campaigns = [
+
+        var messaging_campaigns = [
             {id :'Select One'},
             {id : 'Campaign 1'},
             {id : 'Campaign 2'},
             {id : 'Campaign 3'}
         ];
-        $scope.lead.messaging_campaign = $scope.messaging_campaigns[0];
-        $scope.resetLead = function () {
-            $scope.lead = {};
-        };
+        $scope.messaging_campaigns = messaging_campaigns;
+        // if (!$scope.submitted) {
+            $scope.lead.messaging_campaign = messaging_campaigns[0];
+        // }
+
+        // $scope.resetLead = function () {
+        //     $scope.lead = {};
+        // };
 
         $scope.uploadLead = function () {
-            console.log($scope.lead);
+            // $scope.submitted = true;
+            // console.log(scope.lead);
         };
-
     }]);
 })();
