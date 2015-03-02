088711f46795f5a8440b0a098c5db7f1d7f65204
Taylor Birkeland <tabirkeland@gmail.com>
2015-03-02 05:23:20 +0000

    merging

--- src/js/controllers/upload_controller.js
+++ src/js/controllers/upload_controller.js
@@ -1,28 +1,33 @@
 (function () {
     var app = angular.module('App.controllers.Upload', []);
     app.controller('UploadController', ['$scope', function($scope) {
+        $scope.response = '';
         $scope.submitted = false;
         $scope.lead = {};
 
-
         var messaging_campaigns = [
-            {id :'Select One'},
-            {id : 'Campaign 1'},
-            {id : 'Campaign 2'},
-            {id : 'Campaign 3'}
+            {id : 1, name : 'Select One'},
+            {id : 2, name : 'Campaign 1'},
+            {id : 3, name : 'Campaign 2'},
+            {id : 4, name : 'Campaign 3'}
         ];
-        $scope.messaging_campaigns = messaging_campaigns;
-        // if (!$scope.submitted) {
-            $scope.lead.messaging_campaign = messaging_campaigns[0];
-        // }
 
-        // $scope.resetLead = function () {
-        //     $scope.lead = {};
-        // };
+        $scope.messaging_campaigns = messaging_campaigns;
+        if (!$scope.submitted) {
+            $scope.lead.messaging_campaign = messaging_campaigns[0];
+        }
+
+        $scope.resetLead = function () {
+            $scope.lead = {};
+        };
 
         $scope.uploadLead = function () {
-            // $scope.submitted = true;
-            // console.log(scope.lead);
+            $scope.submitted = true;
+            if
+        };
+
+        this.postLead = function () {
+            var lead =
         };
     }]);
 })();
