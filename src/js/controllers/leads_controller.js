a3307e5f8b6b166aa15c056557020891ba914406
Taylor Birkeland <tabirkeland@gmail.com>
2015-03-08 07:58:56 +0000

    merging

--- src/js/controllers/leads_controller.js
+++ src/js/controllers/leads_controller.js
@@ -2,7 +2,16 @@
     var app = angular.module('App.controllers.Leads', []);
     app.controller('LeadsController', ['$http', '$scope', function($http, $scope) {
     	$scope.response = {};
-        $scope.tabs = [];
+        $scope.tabs = {};    
+
+        $scope.toggleOpen = function(id) {
+            for (var i in $scope.tabs) {
+                if ($scope.tabs[i] != 'undefined' && i != id) {
+                    $scope.tabs[i] = false;
+                }
+            }
+            $scope.tabs[id] = !$scope.tabs[id];
+        };        
 
         // HTTP GET leads
 		$http.get("http://54.69.7.81/taylor/html/app_api/methods/get/leads/?user_id=1")
@@ -24,6 +33,5 @@
     		.error(function (data, status, headers, config) {
     			console.log('FATAL ERROR');
     		});
-
     }]);
 })();
