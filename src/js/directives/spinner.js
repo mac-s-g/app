0d1fdebd0252bc55554004978cfad5d5afd3766c
Taylor Birkeland <tabirkeland@gmail.com>
2015-03-10 05:22:14 +0000

    Loading spinner

--- src/js/directives/spinner.js
+++ src/js/directives/spinner.js
@@ -1,25 +1,14 @@
 (function(){
-    var app = angular.module('App.directives.loadingSpinner', []);
+    var app = angular.module('App.directives.loadingSpinner', ['App.controllers.Main']);
     app.directive('loadingSpinner', function() {
-        'use strict';
-
         return {
           restrict: 'A',
-          replace: true,
           transclude: true,
-          // scope: {
-          //   datasource: '=viewLoading'
-          // },
           templateUrl: 'loading.html',
-          link: function(scope, element, attrs) {
+          compile: function compile(tElement, tAttrs, transclude) {
             angular.element(document).ready(function () {
                 var spinner = new Spinner().spin();
-                // His
-                // var loadingContainer = element.find('.loading-spinner-container')[0];
-
-                // Mine
                 var loadingContainer = document.getElementsByClassName("loading-spinner-container")[0];
-
                 loadingContainer.appendChild(spinner.el);
             });
           }
