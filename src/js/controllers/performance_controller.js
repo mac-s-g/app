07a1f82d672e8e718ace391f3dca01a26d673247
Mac <mac.gainor@gmail.com>
2015-03-15 20:40:18 +0000

    fixed performance page

--- src/js/controllers/performance_controller.js
+++ src/js/controllers/performance_controller.js
@@ -7,7 +7,7 @@
         app.charts  = that.charts;
         console.log(app.charts);
         //draw charts
-        drawCharts(that.charts);
+        app.charts  = drawCharts(that.charts);
 
         function getCharts() {
           //chartist data object for each chart
@@ -27,6 +27,20 @@
                 fullHeight: true,
               }
             },
+            {
+              'id': 'chart-2',
+              'type': 'line',
+              'options': {
+                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                series: [
+                  [5, 5, 7, 6.5, 8],
+                  [4.5, 6, 4.5, 4, 5.5],
+                  [2.5, 3.5, 6, 5, 4]
+                ],
+                fullWidth: true,
+                fullHeight: true,
+              }
+            },
           ];
 
           return charts;
@@ -34,6 +48,7 @@
 
         function drawCharts(charts) {
           var chart;
+          drawn = [];
           console.log('drawing charts');
           for (var i = 0; i < charts.length; i++) {
             chart = charts[i];
@@ -44,7 +59,10 @@
               default:
                 console.log('unknown chart type');
             }
+            drawn.push(chart);
           }
+          //return array of drawn objects
+          return drawn;
         }
 
     }]);
@@ -55,6 +73,7 @@
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
+                console.log(app.charts);
                 element.bind('click', function() {
                     for (var i = 0; i < app.charts.length; i++) {
                       app.charts[i].update();
